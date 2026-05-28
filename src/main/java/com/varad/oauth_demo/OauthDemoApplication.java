7063f3e68f2a4c389e6bfc2ef3a464008e40995c
Varad-Dev-17 <Varad.Mule@cybaemtech.com>
2026-05-28 15:32:51 +0530

    Added Lombok and SLF4J logger

--- src/main/java/com/varad/oauth_demo/OauthDemoApplication.java
+++ src/main/java/com/varad/oauth_demo/OauthDemoApplication.java
@@ -1,18 +1,16 @@
 package com.varad.oauth_demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class OauthDemoApplication {
-    public static final Logger logger = LoggerFactory.getLogger(OauthDemoApplication.class);
-	public static void main(String[] args) {
-
+    public static void main(String[] args) {
         SpringApplication.run(OauthDemoApplication.class, args);
-        logger.info("OAuth Demo Application Started.");
-
-	}
-
+        log.info("OAuth Demo Application Started.");
+    }
 }
--- src/main/java/com/varad/oauth_demo/controller/HelloController.java
+++ src/main/java/com/varad/oauth_demo/controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.varad.oauth_demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
@@ -30,6 +32,7 @@ public String home(){
     public String hello(@AuthenticationPrincipal OAuth2User user){
 
         System.out.println(user.getAttributes());
+        log.info("user attributes: "+user.getAttributes());
 
         String name = user.getAttribute("name");
 
